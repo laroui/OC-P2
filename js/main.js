f45cc68ff33558997af3f256e81ad7d39b7b0674
LAROUI Nacim <l.nassimo@gmail.com>
2020-06-06 15:43:55 +0200

    cv-oc first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Scroll to specific values
+// scrollTo is the same
+
+
+
+window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position
+window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+});
+
